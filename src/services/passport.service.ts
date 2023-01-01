5a00665df165974115b952da7e26bf5626bcef65
Mike Guijarro <a01612042@itesm.mx>
2023-01-01 01:12:28 -0600

    Refactor passport.service class

--- src/services/passport.service.ts
+++ src/services/passport.service.ts
@@ -1,35 +1,57 @@
 import { Request } from 'express';
 import passport from 'passport';
 import passportLocal from 'passport-local';
+import { ExpressUser } from '../common/types/ExpressUser';
 import { MODELS } from '../constants/models';
 import { STRATEGIES } from '../constants/strategies';
 import { Partner } from '../models/partner';
 import { hashPassword, matchPassword } from '../util/helpers';
-
-const saltRounds = 10;
+import { CompanyService } from './company.service';
+import { CompanySequelizeRepository } from './repositories/implementation/sequelize/company.repository';
 
 export class PassportService {
   public passport: passport.PassportStatic = passport;
 
   constructor() {
-    this.passport.serializeUser((user: any, cb: Function) => {
+    this.initPassport();
+    this.initStrategies();
+  }
+
+  initPassport() {
+    const companyRepository = new CompanySequelizeRepository();
+    const companyService = new CompanyService(companyRepository);
+    this.passport.serializeUser(async (user: any, cb: Function) => {
       // Only called on login
-      const coreUser = {
+      const coreUser: any = {
         id: user.id,
         firstName: user.firstName,
         middleName: user.middleName,
         lastName: user.lastName,
         email: user.email,
+        company: null,
       };
+
+      const companies = await companyService.findAll(user.id);
+
+      if (companies.length > 0) {
+        const company = companies[0];
+        coreUser.company = company;
+      }
+
       cb(null, coreUser);
     });
 
-    this.passport.deserializeUser((user: Express.User, cb: Function) => {
+    this.passport.deserializeUser(async (user: ExpressUser, cb: Function) => {
       // Called on all requests
+      if (!user.company) {
+        const companies = await companyService.findAll(parseInt(user.id));
+        if (companies.length > 0) {
+          const company = companies[0];
+          user.company = company;
+        }
+      }
       return cb(null, user);
     });
-
-    this.initStrategies();
   }
 
   initStrategies() {
