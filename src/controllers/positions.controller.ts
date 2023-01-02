48c8701ef353d061866a958188baaae045202435
Mike Guijarro <a01612042@itesm.mx>
2023-01-02 17:16:33 -0600

    Add Positions controller

---
+++ src/controllers/positions.controller.ts
@@ -0,0 +1,88 @@
+import { Response } from 'express';
+import { PositionService } from '../services/positions.service';
+import { PositionSequelizeRepository } from '../services/repositories/implementation/sequelize/position.repository';
+import { AuthRequest } from './interfaces/AuthRequest';
+
+export class PositionsController {
+  private readonly positionService: PositionService;
+  constructor() {
+    const positionSequelizeRepository = new PositionSequelizeRepository();
+    this.positionService = new PositionService(positionSequelizeRepository);
+  }
+
+  public getPosition = async (
+    req: AuthRequest,
+    res: Response
+  ): Promise<void> => {
+    const companyId = parseInt(req.user.company.id);
+    const positionId = parseInt(req.params.positionId);
+    const position = await this.positionService.findOne(positionId, companyId);
+    res.status(200).json({
+      success: true,
+      data: position,
+    });
+  };
+
+  public getPositions = async (
+    req: AuthRequest,
+    res: Response
+  ): Promise<void> => {
+    const companyId = parseInt(req.user.company.id);
+    const positions = await this.positionService.findAll(companyId);
+    res.status(200).json({
+      success: true,
+      data: positions,
+    });
+  };
+
+  public postPosition = async (
+    req: AuthRequest,
+    res: Response
+  ): Promise<void> => {
+    const body = req.body;
+    const companyId = parseInt(req.user.company.id);
+    const newPosition = await this.positionService.create({
+      ...body,
+      companyId: companyId,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: newPosition,
+    });
+  };
+
+  public putPosition = async (
+    req: AuthRequest,
+    res: Response
+  ): Promise<void> => {
+    const body = req.body;
+    const positionId = parseInt(req.params.positionId);
+    const companyId = parseInt(req.user.company.id);
+    const updatedPosition = await this.positionService.update(
+      body,
+      positionId,
+      companyId
+    );
+    res.status(200).json({
+      success: true,
+      data: updatedPosition,
+    });
+  };
+
+  public deletePosition = async (
+    req: AuthRequest,
+    res: Response
+  ): Promise<void> => {
+    const positionId = parseInt(req.params.positionId);
+    const companyId = parseInt(req.user.company.id);
+    const deletedPosition = await this.positionService.delete(
+      positionId,
+      companyId
+    );
+    res.status(200).json({
+      success: true,
+      data: {},
+    });
+  };
+}
