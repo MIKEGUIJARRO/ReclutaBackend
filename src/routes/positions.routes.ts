1703e797f178f17fd438302c6149d840649b0d9c
Mike Guijarro <a01612042@itesm.mx>
2023-01-12 20:50:46 -0600

    Add kanbanPositionsRoutes in positionsRoutes

--- src/routes/positions.routes.ts
+++ src/routes/positions.routes.ts
@@ -3,13 +3,15 @@ import { BaseRoutes } from '../common/routes/base.routes';
 import { PositionsController } from '../controllers/positions.controller';
 import { partnerAuthCheck } from '../middlewares/auth';
 import { asyncHandler } from '../util/asyncHandler';
+import { KanbanPositionRoutes } from './kanbanPosition.routes';
 
 export class PositionsRoutes implements BaseRoutes<PositionsController> {
   readonly name: string = 'positions';
   readonly controller: PositionsController = new PositionsController();
-  readonly router: Router = Router();
+  readonly router: Router = Router({ mergeParams: true });
 
   constructor() {
+    this.initChildRoutes();
     this.initRoutes();
   }
 
@@ -24,5 +26,10 @@ export class PositionsRoutes implements BaseRoutes<PositionsController> {
       .get(partnerAuthCheck, asyncHandler(this.controller.getPosition))
       .put(partnerAuthCheck, asyncHandler(this.controller.putPosition))
       .delete(partnerAuthCheck, asyncHandler(this.controller.deletePosition));
+  }
+
+  initChildRoutes(): void {
+    const kanbanPositionRoutes = new KanbanPositionRoutes();
+    this.router.use('/:positionId/kanban', kanbanPositionRoutes.router);
   }
 }
