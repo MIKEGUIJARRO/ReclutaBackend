c1cfb8f8b4ff25a41b6b99f89adda2c41885efaf
Mike Guijarro <a01612042@itesm.mx>
2023-01-18 21:28:21 -0600

    Add more packages to application

--- src/services/repositories/interfaces/candidate.ts
+++
@@ -1,4 +0,0 @@
-import { SequelizeRead } from '../implementation/sequelize/interfaces/read';
-import { SequelizeWrite } from '../implementation/sequelize/interfaces/write';
-
-export interface CandidateRepository extends SequelizeRead, SequelizeWrite {}
