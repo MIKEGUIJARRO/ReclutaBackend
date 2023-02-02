8949a4825709f822eb5d4e0f632d67e4a03b280d
Mike Guijarro <a01612042@itesm.mx>
2023-02-01 20:33:25 -0600

    Refactor CandidatesStatusRepository

--- src/services/repositories/interfaces/candidatesStatus.ts
+++ src/services/repositories/interfaces/candidatesStatus.ts
@@ -1,6 +1,6 @@
 import { SequelizeRead } from '../implementation/sequelize/interfaces/read';
 import { SequelizeWrite } from '../implementation/sequelize/interfaces/write';
-
+import { CandidateStatusAttributes } from '../../../models/candidateStatus';
 export interface CandidatesStatusRepository
-  extends SequelizeRead,
-    SequelizeWrite {}
+  extends SequelizeRead<CandidateStatusAttributes>,
+    SequelizeWrite<CandidateStatusAttributes> {}
