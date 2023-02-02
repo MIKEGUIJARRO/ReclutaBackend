1e633b26d2f216fab7c5cb2c7c23d72b5f0a3221
Mike Guijarro <a01612042@itesm.mx>
2023-02-01 21:02:00 -0600

    Refactor Company

--- src/services/repositories/interfaces/company.ts
+++ src/services/repositories/interfaces/company.ts
@@ -1,6 +1,6 @@
 import { SequelizeRead } from '../implementation/sequelize/interfaces/read';
 import { SequelizeWrite } from '../implementation/sequelize/interfaces/write';
-import { CompanyAttributes } from '../../../models/company';
+import { Company } from '../../../models/company';
 export interface CompanyRepository
-  extends SequelizeRead<CompanyAttributes>,
-    SequelizeWrite<CompanyAttributes> {}
+  extends SequelizeRead<Company>,
+    SequelizeWrite<Company> {}
