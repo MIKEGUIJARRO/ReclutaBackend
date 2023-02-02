96acda21add5231005007af825df4e7245871c73
Mike Guijarro <a01612042@itesm.mx>
2023-02-01 21:02:20 -0600

    Refactor Positions

--- src/services/repositories/interfaces/positions.ts
+++ src/services/repositories/interfaces/positions.ts
@@ -1,6 +1,6 @@
 import { SequelizeRead } from '../implementation/sequelize/interfaces/read';
 import { SequelizeWrite } from '../implementation/sequelize/interfaces/write';
-import { PositionAttributes } from '../../../models/position';
+import { Position } from '../../../models/position';
 export interface PositionsRepository
-  extends SequelizeRead<PositionAttributes>,
-    SequelizeWrite<PositionAttributes> {}
+  extends SequelizeRead<Position>,
+    SequelizeWrite<Position> {}
